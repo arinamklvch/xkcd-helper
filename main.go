d1cb0a6c5320d01dc1f43a25451defb564007a9b
Arina Mikhalevich <arinamikhalevich@gmail.com>
2026-07-20 13:18:19 +0300

    Add worker pool for downloading

--- main.go
+++ main.go
@@ -13,40 +13,66 @@ type Comic struct {
 	Title string `json:"safe_title"`
 }
 
-func main() {
-	left := flag.Int("first", 1, "left boundary")
-	right := flag.Int("last", 100, "right boundary")
-	flag.Parse()
-	for *left <= *right {
-		url := fmt.Sprintf("https://xkcd.com/%d/info.0.json", *left)
-		response, err1 := http.Get(url)
-		if err1 != nil {
-			fmt.Println("Request error:", err1)
-			continue
+type Response struct {
+	str string
+	err error
+}
+
+func downloadComic(comicNum <-chan int, result chan<- Response) {
+	for n := range comicNum {
+		url := fmt.Sprintf("https://xkcd.com/%d/info.0.json", n)
+		response, err := http.Get(url)
+		if err != nil {
+			result <- Response{err: fmt.Errorf("Request error: %w", err)}
+			return
 		}
 		defer response.Body.Close()
-		if response.StatusCode == http.StatusNotFound && *left != 404 {
+		if response.StatusCode == http.StatusNotFound && n != 404 {
 			return
 		}
 		if response.StatusCode != http.StatusOK {
-			fmt.Println("Error:", response.Status)
-			*left++
-			continue
+			result <- Response{err: fmt.Errorf("Unexpected status code: %s", response.Status)}
+			return
 		}
-		body, err1 := io.ReadAll(response.Body)
-		if err1 != nil {
-			fmt.Println("Error reading response:", err1)
-			*left++
-			continue
+		body, err := io.ReadAll(response.Body)
+		if err != nil {
+			result <- Response{err: fmt.Errorf("Error reading response: %w", err)}
+			return
 		}
 		var comic Comic
-		err2 := json.Unmarshal(body, &comic)
-		if err2 != nil {
-			fmt.Println("Error decoding JSON:", err2)
-			*left++
-			continue
+		err = json.Unmarshal(body, &comic) // err != nil, err = "Failed unmarshal: unexpected ..."
+		if err != nil {
+			result <- Response{err: fmt.Errorf("Error decoding JSON: %w", err)} // err after errorf will be like "Error decoding JSON: 'Failed unmarshal: ...'
+			return
 		}
-		fmt.Printf("Comic number: %d. Comic title: \"%s\".\n", comic.Num, comic.Title)
-		*left++
+		result <- Response{str: fmt.Sprintf("Comic number: %d. Comic title: \"%s\".", comic.Num, comic.Title)}
+	}
+
+}
+
+func main() {
+	maxWorkers := flag.Int("workers", 5, "max number of work")
+	left := flag.Int("first", 1, "left boundary")
+	right := flag.Int("last", 100, "right boundary")
+	flag.Parse()
+	totalCnt := *right - *left + 1
+	comicNum := make(chan int, totalCnt)
+	result := make(chan Response, totalCnt)
+
+	for w := 1; w <= *maxWorkers; w++ {
+		go downloadComic(comicNum, result)
+	}
+
+	for n := *left; n <= *right; n++ {
+		comicNum <- n
+	}
+	close(comicNum)
+
+	for i := 1; i <= totalCnt; i++ {
+		res := <-result
+		if res.err != nil {
+			fmt.Printf("%s", res.err.Error())
+		}
+		fmt.Println(res.str)
 	}
 }
