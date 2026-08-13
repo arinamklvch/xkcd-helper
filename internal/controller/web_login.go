5cc2d3210f99064b56f7b284502554c5fdb754d5
Arina Mikhalevich <arinamikhalevich@gmail.com>
2026-08-13 16:43:21 +0300

    Improve web page styling

--- internal/controller/web_login.go
+++ internal/controller/web_login.go
@@ -7,6 +7,14 @@ import (
 
 func (h *Handler) webLogin(w http.ResponseWriter, r *http.Request) {
 	// [sign in] -- отправляется POST /login
-	tmpl := template.Must(template.ParseFiles("templates/login.html"))
-	tmpl.Execute(w, nil)
+	tmpl, err := template.ParseFiles("templates/login.html")
+	if err != nil {
+		http.Error(w, "failed to parse template", http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "failed to render template", http.StatusInternalServerError)
+		return
+	}
 }
